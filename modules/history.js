429a368f754da979e9b05cd397cbf03d2e3da891
Mfweb <h@mfweb.pw>
2019-03-01 12:01:47 +0800

    历史记录记录哪个岛

--- modules/history.js
+++ modules/history.js
@@ -1,4 +1,5 @@
 import SQLite from 'react-native-sqlite-storage'
+import { configDynamic } from './config'
 
 var __historySQLite = null;
 function __clearHistory(tableName) {
@@ -14,7 +15,7 @@ function __clearHistory(tableName) {
 function historyTableInit(tableName) {
     return new Promise((resolve, reject) => {
         __historySQLite.transaction((tx) => {
-            tx.executeSql(`CREATE TABLE IF NOT EXISTS ${tableName} (id INTEGER PRIMARY KEY AUTOINCREMENT, tid INTEGER DEFAULT 0, cache TEXT, addtime INTEGER DEFAULT 0)`, [], (tx, results) => {
+            tx.executeSql(`CREATE TABLE IF NOT EXISTS ${tableName} (id INTEGER PRIMARY KEY AUTOINCREMENT, island VARCHAR(5), tid INTEGER DEFAULT 0, cache TEXT, addtime INTEGER DEFAULT 0)`, [], (tx, results) => {
                 console.log(results);
                 resolve();
             });
@@ -25,8 +26,8 @@ function historyTableInit(tableName) {
 function init() {
     return new Promise((resolve, reject)=>{
         __historySQLite = SQLite.openDatabase({name: 'history.db', location: 'default'}, async ()=>{
-            //await __clearHistory('UserBrowseHistory');
-            //await __clearHistory('UserReplyHistory');
+//            await __clearHistory('UserBrowseHistory');
+//            await __clearHistory('UserReplyHistory');
             await historyTableInit('UserBrowseHistory');
             await historyTableInit('UserReplyHistory');
         }, (e)=>{
@@ -38,7 +39,7 @@ function init() {
 function addNewHistory(mode, detail, time) {
     return new Promise((resolve, reject) => {
         __historySQLite.transaction((tx) => {
-            tx.executeSql(`INSERT INTO ${mode==='browse'?'UserBrowseHistory':'UserReplyHistory'}(tid, cache, addtime) VALUES(${detail.id},'${JSON.stringify(detail)}',${time})`, [], (tx, results) => {
+            tx.executeSql(`INSERT INTO ${mode==='browse'?'UserBrowseHistory':'UserReplyHistory'}(island, tid, cache, addtime) VALUES('${configDynamic.islandMode}',${detail.id},'${JSON.stringify(detail)}',${time})`, [], (tx, results) => {
                 console.log(results);
                 resolve();
             });
